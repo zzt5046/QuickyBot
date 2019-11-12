80a5520148518dc6aac8d8b89defe5450e9efc37
Matthew Goulding <my60gb@protonmail.com>
2019-11-11 23:02:51 -0500

    MY API KEY

--- src/main/java/main/Bot.java
+++ src/main/java/main/Bot.java
@@ -9,7 +9,7 @@ import javax.security.auth.login.LoginException;
 
 public class Bot {
 
-    private static final String token = "NjQzMzA1NDA1MzYzMjU3MzU1.Xcji5w.seIctO62PZM7gizi6wh3NlkxSjs";
+    private static final String token = "NjQzNjU2Njc0Mjc1NzUzOTg3.XcoqkQ.-V6cQK7-faWnREtAjIgxzmpbtTY";
     private final String botURL = "https://discordapp.com/api/oauth2/authorize?client_id=643305405363257355&permissions=129024&scope=bot";
 
     public static void main(String[] args) throws LoginException {
