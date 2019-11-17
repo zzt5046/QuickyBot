9b3826b483b8cdaf9b736ead430e565b2fc6f47e
Zach <zthomas2323@gmail.com>
2019-11-16 22:22:53 -0500

    Update Bot.java

--- src/main/java/main/Bot.java
+++ src/main/java/main/Bot.java
@@ -1,7 +1,6 @@
 package main;
 
 import commands.BasicCommand;
-import events.BasicEvent;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 
@@ -12,7 +11,6 @@ public class Bot {
     public static void main(String[] args) throws LoginException {
         PropertyLoader propertyLoader = new PropertyLoader();
         JDA jda = new JDABuilder(propertyLoader.getProperty("token")).build();
-        jda.addEventListener(new BasicEvent());
         jda.addEventListener(new BasicCommand());
     }
 }
